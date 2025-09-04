20518b58e3220f00d7eb8846def10b355d285541
Jonathan Azevedo <jonathanbazevedo@hotmail.com>
2025-09-04 15:57:56 -0300

    adding files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+// export default function RootLayout() {
+//   return <Stack />;
+// }
+
+
+
+// TELA EXCLUIDA NO INICIO
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,41 @@
-import { Text, View } from "react-native";
+import { Background } from "@react-navigation/elements";
+import { Image, Text, View, StyleSheet } from "react-native";
+import { Icon } from 'lucide-react-native'
 
+
+// export default function App() 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>TESTE INICIANDO PROJETO COM REACT NATIVE</Text>
+
+      <Image source={require('./pomodoro.png')} />
+      <View style={styles.actions}></View>
+      
     </View>
+    
   );
 }
 
+
+// estilos layout 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#021123',
+    gap: 40,
+  },
+  actions: {
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: '#14448080',
+    width: '80%',
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: '#144480',
+  }
+
+});   
